7d605a641d3b79085cfae6fcddda6a45ab3e6fba
ttyagianjali <74582328+ttyagianjali@users.noreply.github.com>
2021-01-17 09:16:28 -0500

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+let ItemList= $('ul');
+itemList.addClass('.unorderedList');
+$('.unorderedList').append(<li>#</li>);
+let inputValue= $('#input').val();
+li.append(inputValue);
+$('li').addClass('.strike');
+$('li').addClass('.delete');
+li.on("click", function() {
+    li.addClass("strike");
+  });
