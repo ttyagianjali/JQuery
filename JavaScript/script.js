3f2c1cebb0efa150cefa61760db8d8dcce996f1a
ttyagianjali <74582328+ttyagianjali@users.noreply.github.com>
2021-01-22 05:52:34 -0500

    jQuery app
    a small app to display the understanding of jQuerry. In this app, one can add items to  a to-do list, delete those items, reorder those items and cancel those items.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,38 @@
-let ItemList= $('ul');
-itemList.addClass('.unorderedList');
-$('.unorderedList').append(<li>#</li>);
-let inputValue= $('#input').val();
-li.append(inputValue);
-$('li').addClass('.strike');
-$('li').addClass('.delete');
-li.on("click", function() {
-    li.addClass("strike");
-  });
+function newItem(){
+// adding new item to the list
+
+    let li= $('<li></li>');
+    let inputValue= $('#input').val();
+    li.append(inputValue);
+
+    if(inputValue === ''){
+        alert('You must enter something!');
+    }
+    else{
+        $('#list').append(li);
+    }
+
+    // crossing out item from the list of items.
+    function crossOut(){
+        li.toggleClass('strike');
+    }
+    li.on('dblclick', crossOut);
+    
+
+    // adding the delete button
+    let crossOutButton= $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+    crossOutButton.on('click', deleteListItem);
+        function deleteListItem(){
+            li.addClass('delete')
+        }
+
+    
+
+    $('#list').sortable();
+
+}
+    
+
+    
